96c995308d36487e5ac671fea14a616326376fd9
PapaniaP <papaniap11@gmail.com>
2023-11-06 09:55:38 +0100

    added assets/fonts

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,25 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./ChatsPage.css";
+
+const ChatsPage: React.FC = () => {
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>ChatsPage</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonHeader collapse="condense">
+					<IonToolbar>
+						<IonTitle size="large">ChatsPage</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<ExploreContainer name="ChatsPage" />
+			</IonContent>
+		</IonPage>
+	);
+};
+
+export default ChatsPage;
