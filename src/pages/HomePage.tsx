aad229bd7d1b1ad23c3d1b28c8ec11835083e519
PapaniaP <papaniap11@gmail.com>
2023-11-06 18:10:58 +0100

    created home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,38 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useState, useEffect } from "react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./HomePage.css";
 import CardNextGame from "../components/CardNextGame";
 import GameList from "../components/CardNextGame";
+import { GameInfo } from "../components/CardNextGame";
 
 const HomePage: React.FC = () => {
+	const [games, setGames] = useState<GameInfo[]>([]);
+
+	useEffect(() => {
+		const url = `https://swish-cc699-default-rtdb.europe-west1.firebasedatabase.app/games.json`;
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				return response.json();
+			})
+			.then((data) => {
+				// Convert object of objects into an array
+				const loadedGames = Object.keys(data).map((key) => {
+					return {
+						id: key,
+						...data[key],
+					};
+				});
+				setGames(loadedGames);
+			})
+			.catch((error) => {
+				console.error("Error fetching data: ", error);
+			});
+	}, []);
+
 	return (
 		<IonPage>
 			<IonHeader className="ion-no-border">
@@ -18,7 +46,12 @@ const HomePage: React.FC = () => {
 						<IonTitle size="large">Your next game</IonTitle>
 					</IonToolbar>
 				</IonHeader>
-				<CardNextGame />
+				{games.map((game) => (
+					<CardNextGame
+						key={game.id}
+						gameInfo={game}
+					/>
+				))}
 			</IonContent>
 		</IonPage>
 	);
