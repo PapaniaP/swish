383b864bac882bd3eafcc59b388239d5ef048f0e
PapaniaP <papaniap11@gmail.com>
2023-11-07 14:19:49 +0100

    updating styling for card still, more

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,18 +1,24 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
 
 interface ContainerProps {
-  name: string;
-  subheading: string;
+	name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name, subheading }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <strong>{subheading}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+	return (
+		<div className="container">
+			<p>
+				Explore{" "}
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://ionicframework.com/docs/components"
+				>
+					UI Components
+				</a>
+			</p>
+		</div>
+	);
 };
 
 export default ExploreContainer;
