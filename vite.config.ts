427de7db8a6183bc5b654212893027835ecc2b1a
PapaniaP <papaniap11@gmail.com>
2023-11-10 19:21:43 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// this on might be "import react from "@vitejs/plugin-react";" in your solution
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+export default defineConfig(({ command }) => {
+	const config = {
+		plugins: [react()],
+		base: "/",
+	};
+
+	if (command !== "serve") {
+		config.base = "/swish-fork/";
+	}
+
+	return config;
+});
